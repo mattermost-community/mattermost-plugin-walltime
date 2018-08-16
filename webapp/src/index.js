fb356230f4f436c9d7040a3564abb717459a6bc7
Martin Kraft <martinkraft@gmail.com>
2018-08-16 14:04:16 -0400

    Iterates over multiple dates in a message.

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -13,32 +13,44 @@ export default class Plugin {
             const posterTimezome = timeZoneForUser(postUser);
             const currentUserTimezone = timeZoneForUser(currentUser);
             const referenceDate = moment(post.create_at);
-            const parseResult = chrono.parse(message, referenceDate)[0];
-            const parsedMessageStartDate = parseResult.start.date();
-            const parsedMessageEndDate = parseResult.end ? parseResult.end.date() : null;
-            const parsedText = parseResult.text;
-            if (!parsedMessageStartDate) {
+            const parseResults = chrono.parse(message, referenceDate);
+
+            if (!parseResults.length) {
                 return message;
             }
-            const parsedMessageStartDateMoment = moment(parsedMessageStartDate).subtract(-moment().utcOffset() + moment().tz(posterTimezome).utcOffset(), 'minutes');
-            let parsedMessageEndDateMoment;
-            if (parsedMessageEndDate) {
-                parsedMessageEndDateMoment = moment(parsedMessageEndDate).subtract(-moment().utcOffset() + moment().tz(posterTimezome).utcOffset(), 'minutes');
-            }
-            const currentUserMessageStartDate = parsedMessageStartDateMoment.tz(currentUserTimezone);
-            let currentUserMessageEndDate;
-            let timezoneMessage;
-            if (parsedMessageEndDateMoment) {
-                currentUserMessageEndDate = parsedMessageEndDateMoment.tz(currentUserTimezone);
 
-                if (currentUserMessageStartDate.isSame(currentUserMessageEndDate, 'day')) {
-                    timezoneMessage = `${message.replace(parsedText, `\`${parsedText}\``)} *(${currentUserMessageStartDate.format('LLLL')} - ${currentUserMessageEndDate.format('LT z')})*`;
-                } else {
-                    timezoneMessage = `${message.replace(parsedText, `\`${parsedText}\``)} *(${currentUserMessageStartDate.format('LLLL z')} - ${currentUserMessageEndDate.format('LLLL z')})*`;
+            let timezoneMessage = message;
+
+            parseResults.forEach((parseResult) => {
+                const parsedMessageStartDate = parseResult.start.date();
+
+                const parsedMessageEndDate = parseResult.end ? parseResult.end.date() : null;
+
+                const parsedText = parseResult.text;
+
+                const parsedMessageStartDateMoment = moment(parsedMessageStartDate).subtract(-moment().utcOffset() + moment().tz(posterTimezome).utcOffset(), 'minutes');
+
+                let parsedMessageEndDateMoment;
+                if (parsedMessageEndDate) {
+                    parsedMessageEndDateMoment = moment(parsedMessageEndDate).subtract(-moment().utcOffset() + moment().tz(posterTimezome).utcOffset(), 'minutes');
                 }
-            } else {
-                timezoneMessage = `${message.replace(parsedText, `\`${parsedText}\``)} *(${currentUserMessageStartDate.format('LLLL z')})*`;
-            }
+
+                const currentUserMessageStartDate = parsedMessageStartDateMoment.tz(currentUserTimezone);
+
+                let currentUserMessageEndDate;
+                if (parsedMessageEndDateMoment) {
+                    currentUserMessageEndDate = parsedMessageEndDateMoment.tz(currentUserTimezone);
+
+                    if (currentUserMessageStartDate.isSame(currentUserMessageEndDate, 'day')) {
+                        timezoneMessage = `${timezoneMessage.replace(parsedText, `\`${parsedText}\``)} *(${currentUserMessageStartDate.format('LLLL')} - ${currentUserMessageEndDate.format('LT z')})*`;
+                    } else {
+                        timezoneMessage = `${timezoneMessage.replace(parsedText, `\`${parsedText}\``)} *(${currentUserMessageStartDate.format('LLLL z')} - ${currentUserMessageEndDate.format('LLLL z')})*`;
+                    }
+                } else {
+                    timezoneMessage = `${timezoneMessage.replace(parsedText, `\`${parsedText}\` *(${currentUserMessageStartDate.format('LLLL z')})*`)}`;
+                }
+            });
+
             return timezoneMessage;
         });
     }
