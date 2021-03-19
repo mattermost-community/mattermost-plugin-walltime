fa3d284975e5ab6963ef60854655d4bd4ffa120f
Nick Misasi <nick.misasi@mattermost.com>
2021-03-19 15:56:40 -0400

    Revamping in order to utilize message props to store the parsed time so it can be translated to the relevant timezone on other clients

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,8 +1,25 @@
 import {getCurrentUser} from 'mattermost-redux/selectors/entities/users';
 
+import * as chrono from 'chrono-node';
+
 import PluginId from './plugin_id';
 
-import {convertTimesToLocal} from './time.js';
+const customChrono = chrono.casual.clone();
+customChrono.refiners.push({
+    refine: (context, results) => {
+        // If there is no AM/PM (meridiem) specified,
+        //  let all time between 1:00 - 5:00 be PM (13.00 - 16.00)
+        results.forEach((result) => {
+            if (!result.start.isCertain('meridiem') &&
+                //TODO make these hour values configurable
+                result.start.get('hour') >= 1 && result.start.get('hour') < 5) {
+                result.start.assign('meridiem', 1);
+                result.start.assign('hour', result.start.get('hour') + 12);
+            }
+        });
+        return results;
+    },
+});
 
 function timeZoneForUser(user) {
     let zone;
@@ -38,20 +55,52 @@ function getInfoForCurrentUser(store) {
 }
 
 function messageWillFormat(post, store) {
-    const {message} = post;
+    const {currentUserTimezone, currentUserLocale} = getInfoForCurrentUser(store);
+    let newMessage = post.message;
 
-    const info = getInfoForCurrentUser(store);
-    if (info === null) {
-        return message;
+    // the key contains the matched text, which we'll replace later
+    for (const [key, value] of Object.entries(post.props)) {
+        if (!key.includes('walltime-')) {
+            continue;
+        }
+
+        // Extract the real key from one namespaces with walltime-
+        const parsedKey = key.replace('walltime-', '');
+        const date = new Date(value);
+        const dateString = date.toLocaleString(currentUserLocale, {timeZone: currentUserTimezone});
+        newMessage = newMessage.replace(parsedKey, `${parsedKey} (${dateString})`);
     }
-    const {currentUserTimezone, currentUserLocale} = info;
 
-    return convertTimesToLocal(message, post.create_at, currentUserTimezone, currentUserLocale);
+    return newMessage;
 }
 
+function messageWillPost(post, store) {
+    const {message} = post;
+    const info = getInfoForCurrentUser(store);
+    if (info == null) {
+        return {post};
+    }
+    const result = customChrono.parse(message, new Date());
+    if (result.length === 0) {
+        // if chrono didn't match with anything, just send the post as normal
+        return {post};
+    }
+    result.forEach((date) => {
+        // For each match, add a prop with key of the text that matched and value of the date parsed from match
+        // Namespace the prop with walltime so we don't mess with props from elsewhere
+        const key = `walltime-${date.text}`;
+        post.props = {...post.props, [key]: date.date()};
+    });
+    return {post};
+}
+
+// This is a test message about a meeting next thursday at 1 pm and another date about next tuesday at 4 am
 export default class Plugin {
     // eslint-disable-next-line no-unused-vars
     initialize(registry, store) {
+        registry.registerMessageWillBePostedHook((post) => {
+            return messageWillPost(post, store);
+        });
         registry.registerMessageWillFormatHook((post) => {
             return messageWillFormat(post, store);
         });
